34465ccfccb8484b2f3e8b13afe30e82676578d6
Jivvanraj <jj.raj94@gmail.com>
2019-08-08 17:28:50 +0800

    Daily Update

---
+++ Udemy Projects/4. The Web Developer Bootcamp/Projects/S10. Introduction to JavaScript/Stalker_Exercise/stalker.js
@@ -0,0 +1,6 @@
+var fname = prompt("What is your first name?");
+var lname = prompt("What is your last name?");
+var age = prompt("How old are you?");
+var fullname = fname + " " + lname;
+console.log("Your full name is " + fullname);
+console.log("You are " + age + " years old");
