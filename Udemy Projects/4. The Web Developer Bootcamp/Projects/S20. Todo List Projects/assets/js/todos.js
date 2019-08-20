5ea5ab778154f77a16fae90354f51192bd4104df
Jivvanraj <jj.raj94@gmail.com>
2019-08-20 17:27:51 +0800

    Daily Update

---
+++ Udemy Projects/4. The Web Developer Bootcamp/Projects/S20. Todo List Projects/assets/js/todos.js
@@ -0,0 +1,22 @@
+// Check off specific todos by clicking
+$("li").on("click", function () {
+    $(this).toggleClass("completed");
+})
+
+// Click on X to delete Todo
+$("span").on("click", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
+$("input[type=text]").keypress(function (event) {
+    if (event.which === 13) {
+        // Grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        // Create a new li and add to li
+        $("ul").append("<li>" + todoText + "</li>");
+    }
+})
