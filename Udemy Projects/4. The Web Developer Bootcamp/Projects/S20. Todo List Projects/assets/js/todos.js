ecff98937558c954fefd270914f692944b442852
Jivvanraj <jj.raj94@gmail.com>
2019-08-21 17:26:38 +0800

    Daily Update

--- Udemy Projects/4. The Web Developer Bootcamp/Projects/S20. Todo List Projects/assets/js/todos.js
+++ Udemy Projects/4. The Web Developer Bootcamp/Projects/S20. Todo List Projects/assets/js/todos.js
@@ -1,15 +1,29 @@
+//var modal = document.getElementById("myModal");
+//var btn = document.getElementById("fa-plus");
+//var span = document.getElementsByClassName("close")[0];
+
+//span.onclick = function(){
+//    modal.style.display = "none";
+//}
+
+window.onclick = function(event){
+    if (event.target == modal){
+        modal.style.display = "none";
+    }
+}
+
 // Check off specific todos by clicking
-$("li").on("click", function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
-})
+});
 
 // Click on X to delete Todo
-$("span").on("click", function (event) {
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
-})
+});
 
 $("input[type=text]").keypress(function (event) {
     if (event.which === 13) {
@@ -17,6 +31,16 @@ $("input[type=text]").keypress(function (event) {
         var todoText = $(this).val();
         $(this).val("");
         // Create a new li and add to li
-        $("ul").append("<li>" + todoText + "</li>");
+        $("ul").append("<li><span class='delete'><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+        $(".modal").css("display", "none");
     }
+});
+
+$(".fa-plus").click(function(){
+//    $("input[type='text']").slideToggle();
+    $(".modal").css("display", "block");
+});
+
+$(".close").click(function(){
+    $(".modal").css("display", "none");
 })
